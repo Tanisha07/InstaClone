283f6306134c49f76b96e7f77f2d66a9ad88604b
JumbleRuble <tanishasharda7@gmail.com>
2019-10-10 02:56:37 +0530

    post class, getdataFromFB

--- App/app/src/main/java/com/tani/instaclone/FeedActivity.kt
+++ App/app/src/main/java/com/tani/instaclone/FeedActivity.kt
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_feed.*
 
 class FeedActivity : AppCompatActivity() {
+
+    val emailFromFB: ArrayList<String> = ArrayList()
+    val imageFromFB: ArrayList<String> = ArrayList()
+    val commentFromFB: ArrayList<String> = ArrayList()
+    var firebaseDatabase: FirebaseDatabase? = null
+    var myref : DatabaseReference? = null
+    var adapter : post_class? = null
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater = menuInflater
@@ -27,5 +37,49 @@ class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        myref = firebaseDatabase!!.reference
+
+        adapter = post_class(emailFromFB, imageFromFB, commentFromFB, this)
+        listview.adapter = adapter
+
+        getDataFromFB()
+    }
+
+    fun getDataFromFB(){
+        var newRef = firebaseDatabase!!.getReference("Posts")
+        newRef.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+
+                emailFromFB.clear()
+                commentFromFB.clear()
+                imageFromFB.clear()
+                adapter!!.clear()
+
+                for (snapshot in p0.children){
+                    var hashMap = snapshot.value as HashMap<String,String>
+                    val username = hashMap["userMail"]
+                    val imgurl = hashMap["downloadURL"]
+                    val comment = hashMap["userComment"]
+
+                    if(username != null){
+                        emailFromFB.add(username)
+                    }
+                    if (imgurl!=null){
+                        imageFromFB.add(imgurl)
+                    }
+                    if (comment!=null){
+                        commentFromFB.add(comment)
+                    }
+                    adapter!!.notifyDataSetChanged()
+                }
+            }
+
+        })
+
     }
 }
--- App/app/src/main/java/com/tani/instaclone/post_class.kt
+++ App/app/src/main/java/com/tani/instaclone/post_class.kt
@@ -1,3 +1,21 @@
 package com.tani.instaclone
 
-class post_class ()
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.custom_view.view.*
+
+class post_class (private val userEmail: ArrayList<String>, private val userImg : ArrayList<String>, private val userComment: ArrayList<String>, private val context : Activity ) : ArrayAdapter<String>(context, R.layout.custom_view, userEmail){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layoutInflater = context.layoutInflater
+        val customView = layoutInflater.inflate(R.layout.custom_view, null, true)
+        customView.customEmail.setText(userEmail[position])
+        customView.customComment.setText(userComment[position])
+
+        Picasso.get().load(userImg[position]).resize(300,293).centerCrop().into(customView.customImage)
+
+        return customView
+    }
+}
